b2fe8f4707d029eab5d43be7e222fd36ccd6083e
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-30 23:05:16 +0200

    Update button.py

--- Uploader/button.py
+++ Uploader/button.py
@@ -282,5 +282,5 @@ async def youtube_dl_call_back(bot, update):
 
             )
 
-            logger.info(f"Downloaded in: {str(time_taken_for_download)}")
-            logger.info(f"Uploaded in: {str(time_taken_for_upload)}")
+    logger.info(f"Downloaded in: {str(time_taken_for_download)}")
+    logger.info(f"Uploaded in: {str(time_taken_for_upload)}")
