898c473950ef39db3d5181b3e84bfed5de3225b6
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-31 03:37:28 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -25,7 +25,7 @@
 class Config(object):
 
     # get a token from @BotFather
-    BOT_TOKEN = "5871078140:AAEt8lSMgOxFtDWTtpOFbkcG-uyL8hYnRok"
+    BOT_TOKEN = "5848913964:AAGqQOwJSVH2qP5WhhGZde_Sh9V-ek-eXQ4"
 
     # Get these values from my.telegram.org
     API_ID = 17983098
